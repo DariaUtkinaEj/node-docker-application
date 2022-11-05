d33d812b4fcb8f496f69ec1f68e8debf16317dfa
UtkinaDariaDev <UtkinaDariaDev@proton.me>
2022-11-05 17:42:14 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## NodeDockerApp
+
+This application consists of the frontend and backend parts.
+* Frontend is written with help of the Vue.js framework. (Vite is used to launch
+  front end part of this application).
+* Backend is written using Node.js and Express (api).
+* Database is MySQL. Also, I use Adminer to manage mySQL.
+
+I was working with Docker Node application
+while studying Docker and Docker Compose.
---
+++ api/src/utils/mysqlPool.mjs
@@ -0,0 +1,37 @@
+import mysql from 'mysql2'
+
+const MYSQL_HOST = process.env.MYSQL_HOST || 'mysql'
+const MYSQL_USER = process.env.MYSQL_USER || 'root'
+const MYSQL_PORT = process.env.MYSQL_PORT || '3306'
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || 'password'
+const MYSQL_DB = process.env.MYSQL_DB || 'admin'
+
+const pool = mysql.createPool({
+  connectionLimit: 100,
+  host: MYSQL_HOST,
+  port: MYSQL_PORT,
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DB,
+})
+
+const CREATE_TIMES_TABLE_SQL = `CREATE TABLE IF NOT EXISTS times (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  time TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)`
+
+pool.getConnection((err, connection) => {
+  if (!err) {
+    console.log('Connected to the MySQL DB - ID is ' + connection.threadId)
+    const createTimeTable = CREATE_TIMES_TABLE_SQL
+    connection.query(createTimeTable, (err) => {
+      if (!err) {
+        console.log('Times table was created')
+      }
+    })
+    connection.release()
+  }
+})
+
+export default pool
